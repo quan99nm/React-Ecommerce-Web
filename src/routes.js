946254f287fa215c594ffe8d7ff2b2e4bc397368
Truong <truongdang2k2dn@gmail.com>
2023-09-11 21:34:07 +0700

    update2
    update register + login + about + contact

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 import Home from "./pages/Home";
 import NotFound from "./pages/Notfound";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Contact from "./pages/Contact";
 
 const routes = [
   { path: "", component: <Home /> },
   { path: "home", component: <Home /> },
   { path: "/not-found", component: <NotFound /> },
   { path: "*", component: <NotFound /> },
+  { path: "about", component: <About /> },
+  { path: "login", component: <Login /> },
+  { path: "register", component: <Register /> },
+  { path: "contact", component: <Contact /> },
+
 ];
 export default routes;
